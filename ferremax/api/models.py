8f54d481f94f2d98594288ef016e14396aea5809
italo <it.zapata@duoc.cl>
2025-05-07 18:07:46 -0400

    modelo

--- ferremax/api/models.py
+++ ferremax/api/models.py
@@ -12,7 +12,11 @@ class Producto(model.Model):
     nombre = models.CharField(max_length=100)
     marca = models.ForeignKey(Marca,on_delete= models.CASCADE)
     
-    precio = models.IntegerField 
     codigo_sucursal = models.ForeignKey(Sucursal,on_delete=models.CASCADE)
+
+class Precio(models.Model):
+    producto = models.ForeignKey(Producto, related_name='precios', on_delete=models.CASCADE)
+    fecha = models.DateTimeField()                                 
+    valor = models.DecimalField(max_digits=10, decimal_places=2) 
     
     
