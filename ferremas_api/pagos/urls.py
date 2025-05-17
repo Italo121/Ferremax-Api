50e7660668c5e8cf196a1268538c5c44fa0c7428
javi3r12-code <javiersanm12@gmail.com>
2025-05-16 23:20:29 -0400

    avance 5 2.0

--- ferremas_api/pagos/urls.py
+++ ferremas_api/pagos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import WebpayInitView, WebpayResponse , redirigir_webpay, iniciar_pago
+from .views import WebpayInitView, webpay_response , redirigir_webpay, iniciar_pago
 
 urlpatterns = [
     path('webpay/init/', WebpayInitView.as_view(), name='webpay-init'),
-    path('webpay/response/', WebpayResponse.as_view(), name='webpay-response'),
+    path('webpay/response/', webpay_response, name='webpay-response'),
     path('redirigir_webpay/', redirigir_webpay , name='redigir_webpay'),
     path('webpay/iniciar/', iniciar_pago, name='iniciar-pago'),
 
--- ferremas_api/pagos/views.py
+++ ferremas_api/pagos/views.py
@@ -11,7 +11,7 @@ from django.utils.decorators import method_decorator
 from transbank.common.options import WebpayOptions
 from transbank.common.integration_type import IntegrationType
 from rest_framework import status
-
+from django.http import JsonResponse
 from transbank.common.integration_commerce_codes import IntegrationCommerceCodes
 from transbank.common.integration_api_keys import IntegrationApiKeys
 
@@ -45,13 +45,30 @@ class WebpayInitView(APIView):
             }, status=status.HTTP_400_BAD_REQUEST)
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class WebpayResponse(APIView):
-    def post(self, request):
-        token = request.POST.get("token_ws") 
-        tx = Transaction()
-        result = tx.commit(token)
-        return Response(result)
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+@csrf_exempt
+def webpay_response(request):
+    if request.method == 'POST':
+        token = request.POST.get('token_ws')
+    elif request.method == 'GET':
+        token = request.GET.get('token_ws')
+    else:
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+    if not token:
+        return JsonResponse({'error': 'Token no entregado'}, status=400)
+
+    options = WebpayOptions(
+        commerce_code=IntegrationCommerceCodes.WEBPAY_PLUS,
+        api_key=IntegrationApiKeys.WEBPAY,
+        integration_type=IntegrationType.TEST
+    )
+
+    tx = Transaction(options)
+    result = tx.commit(token)
+    return JsonResponse(result)
     
 def redirigir_webpay(request):
     return render(request, 'redirigir_webpay.html')
